aad360cb7a29f9b2319ae21a3d25098fe6da499a
Naterra <natalinyweb@hotmail.com>
2017-12-13 19:41:10 -0500

    vvdfv

--- index.js
+++ index.js
@@ -22,8 +22,7 @@ app.use(bodyParser.json());
 app.use(cors({ origin: '*' }));
 app.set('port', process.env.PORT || serverPort);
 
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
+
 
 
 
@@ -122,23 +121,23 @@ app.get('/api/generate-fake-data', (req, res) => {
 //
 // });
 
-// if(process.env.NODE_ENV === 'production'){
-//     // Express will serve up production assets
-//     // like our main.js file, or main.css file
-//
-//     app.use(express.static('client/build'));
-//
-//     // Express will serve up the index.html file
-//     // if it doesn't recognize the route
-//     // If express didn't find any route matches listed above,
-//     // will try to find in client/build
-//     // const path = require('path');
-//
-//     app.get('*', (req, res)=>{
-//         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html' ));
-//
-//     });
-// }
+if(process.env.NODE_ENV === 'production'){
+    // Express will serve up production assets
+    // like our main.js file, or main.css file
+
+    app.use(express.static('client/build'));
+
+    // Express will serve up the index.html file
+    // if it doesn't recognize the route
+    // If express didn't find any route matches listed above,
+    // will try to find in client/build
+    const path = require('path');
+
+    app.get('*', (req, res)=>{
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html' ));
+
+    });
+}
 
 app.listen(app.get('port'), () => {
   console.log(`Server is up and running on port ${app.get('port')}`);
