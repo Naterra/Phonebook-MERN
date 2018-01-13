fe5f78fc02da90195fb8c99df18eeb27b91fd24d
Naterra <natalinyweb@hotmail.com>
2018-01-12 22:17:52 -0500

    update API

--- server/db/models/Contact.js
+++ server/db/models/Contact.js
@@ -3,18 +3,19 @@ import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
 const ContactSchema = new Schema({
-  name: { type: String },
-  email: { type: String },
-  company_name: { type: String },
-  category: { type: String },
-  country: { type: String },
-  address: { type: String },
-  state: { type: String },
-  zip: { type: String },
-  cell_phone: { type: String },
-  work_phone: { type: String },
-  fax: { type: String },
-  notes: { type: String },
+	name: { type: String },
+	email: { type: String },
+	image: { type: String },
+	company_name: { type: String },
+	category: { type: String },
+	country: { type: String },
+	address: { type: String },
+	state: { type: String },
+	zip: { type: String },
+	cell_phone: { type: String },
+	work_phone: { type: String },
+	fax: { type: String },
+	notes: { type: String }
 });
 
 mongoose.model('Contact', ContactSchema);
