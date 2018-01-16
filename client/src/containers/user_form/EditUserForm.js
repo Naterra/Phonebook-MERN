87cd3b60825d63d1a7271d9c9885c7b4db9689c1
Naterra <natalinyweb@hotmail.com>
2018-01-15 20:21:07 -0500

    update edit user page

---
+++ client/src/containers/user_form/EditUserForm.js
@@ -0,0 +1,110 @@
+import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
+import validateEmails from '../../utils/validateEmails';
+import {validateZipcode} from "../../utils/validates";
+import _ from 'lodash';
+
+// Redux
+import { connect } from 'react-redux';
+import { saveContact, fetchContact } from '../../actions';
+
+import formFields from './formFields';
+import formFieldsTempl from './formFieldTempl';
+
+class EditUserForm extends Component {
+    componentWillMount() {
+        this.props.fetchContact(this.props.id);
+    }
+
+    formSubmit(values) {
+        this.props.saveContact(values, () => {
+            this.props.onSubmitCallback();
+            // this.props.history.push('/');
+        });
+    }
+
+    renderFields(array = []) {
+        return array.map(field => {
+            return (
+                <Field
+                    key={field.name}
+                    name={field.name}
+                    component={formFieldsTempl}
+                    label={field.label}
+                    type={field.type ? field.type : 'text'}
+                />
+            );
+        });
+    }
+
+    render() {
+        const { handleSubmit } = this.props;
+
+        return (
+            <div className="section">
+                <form onSubmit={handleSubmit(this.formSubmit.bind(this))}>
+                    <div className="row">
+                        <div className="col s6"> {this.renderFields(formFields.pers)}</div>
+                        <div className="col s6">{this.renderFields(formFields.address)}</div>
+                    </div>
+
+                    <br />
+                    <div className="row">
+                        <div className="col s12">{this.renderFields(formFields.contact)}</div>
+                    </div>
+                    <div className="row">
+                        <div className="col s12"> {this.renderFields(formFields.other)}</div>
+                    </div>
+
+                    <Link to="/" className="red btn-flat white-text">
+                        Cansel
+                    </Link>
+
+                    <button type="submit" className="teal btn-flat right white-text">
+                        Save
+                        <i className="material-icons right">done</i>
+                    </button>
+                </form>
+            </div>
+        );
+    }
+}
+
+function mapStateToProps({ selected_contact }) {
+    return {
+        initialValues: selected_contact
+    };
+}
+
+function validate(values) {
+    const errors = {};
+
+    let all_fields = [].concat(formFields.pers, formFields.contact);
+    let required_fields = all_fields.filter(elem => {
+        return elem.reguired === true;
+    });
+
+    _.each(required_fields, ({ name }) => {
+        if (!values[name]) {
+            errors[name] = 'You must provide a value';
+        }
+    });
+
+    errors.Email = validateEmails(values.Email || '');
+
+    if (values.zip) {
+        errors.zip = validateZipcode(values.zip);
+    }
+
+    return errors;
+}
+
+export default connect(mapStateToProps, { fetchContact, saveContact })(
+    reduxForm({
+        form: 'ContactForm',
+        validate: validate,
+        enableReinitialize: true,
+        keepDirtyOnReinitialize: true
+    })(EditUserForm)
+);
--- config/config.js
+++ config/config.js
@@ -1,29 +1,7 @@
-// keys.js - figure out what set of credentials to return
-
 if(process.env.NODE_ENV === 'production'){
     // we are in production
     module.exports = require('./prod');
 }else{
     // we are in developement
-
     module.exports = require('./dev');
 }
-
-
-
-
-
-// import  prod_conf from  './prod';
-// import  dev_conf  from  './dev';
-//
-// let config;
-//
-// if(process.env.NODE_ENV === 'production'){
-//     config =  prod_conf;
-//
-// }else{
-//     config =  dev_conf;
-// }
-//  //console.log(config, 'config');
-//
-// export default config;
